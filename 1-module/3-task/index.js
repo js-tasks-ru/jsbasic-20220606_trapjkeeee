7f8720bc52c1393348649771ebb0c8de1e6ac038
Sergei Shatohin <trapjkeeee@gmail.com>
2022-06-13 18:21:10 +0300

    solution 1-module

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str==false){
+    return str
+  } else{
+    str = str[0].toUpperCase() + str.slice(1)
+    return str
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength-1){
+    str = str.slice(0, maxlength-1) + "…"
+    console.log(str)
+    return str
+  } else{
+    return str
+  }
+  
 }
