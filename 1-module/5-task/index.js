31db8d55eceb09ad05e6e6b81d619b9c26b4b73f
Sergei Shatohin <trapjkeeee@gmail.com>
2022-06-21 19:03:23 +0300

    solution modul-2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,6 @@
 function truncate(str, maxlength) {
   if (str.length > maxlength-1){
     str = str.slice(0, maxlength-1) + "…"
-    console.log(str)
     return str
   } else{
     return str
--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+    for (let value of Object.values(salaries)) {
+      if (value > 0 && value != NaN && value != -Infinity & value != Infinity){
+        sum += value;
+      }
+    }
+    return sum;
 }
